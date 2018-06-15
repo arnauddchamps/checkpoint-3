5fbcd4afdf93639451c7e964330eab7ff470c218
Arnaud Deschamps <arnaud.dchamps@gmail.com>
2018-06-15 09:58:32 +0200

    display target

--- checkpoint-3/src/components/Target.js
+++ checkpoint-3/src/components/Target.js
@@ -1,7 +1,14 @@
 //Component Target display one target
 
 import React from "react";
+import { ListGroupItem } from "reactstrap";
 
-const Target = () => <div>Target</div>;
+const Target = ({ name, status, picture }) => (
+  <ListGroupItem>
+    <h3>{name}</h3>
+    <img src={picture} alt={name} />
+    <h3>{status}</h3>
+  </ListGroupItem>
+);
 
 export default Target;
